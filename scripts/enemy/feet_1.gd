ce2ffa6cec188361afd368d4ac55a1a0435d5c46
Ron-Sungcang <sungcangjoseph@gmail.com>
2026-09-20 03:10:21 -0600

    Enemy Ai now functional

--- scripts/enemy/feet_1.gd
+++ scripts/enemy/feet_1.gd
@@ -2,14 +2,24 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 
+var target = null
+
 func _physics_process(delta: float) -> void:
-	var player = get_parent().get_node("Player")
-	var direction = global_position.direction_to(player.global_position)
-	velocity = direction * SPEED
-	look_at(player.position)
-	move_and_slide()
+	if target != null:
+		var direction = global_position.direction_to(target.global_position)
+		velocity = direction * SPEED
+		look_at(target.global_position)
+		move_and_slide()
+	else:
+		velocity = Vector2.ZERO
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("bullets"):
 		print("Damage enemy")
+
+
+func _on_sight_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		print("Player detecteds")
+		target = body
