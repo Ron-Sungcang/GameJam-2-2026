01b935d969593ce8da0a636a2aa0e2cc88ff6725
Ron-Sungcang <sungcangjoseph@gmail.com>
2026-09-20 04:12:42 -0600

    enemy actually takes damage and dies now

---
+++ scripts/bullet.gd
@@ -0,0 +1,3 @@
+extends RigidBody2D
+
+var damage: int = 25
--- scripts/enemy/feet_1.gd
+++ scripts/enemy/feet_1.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 const SPEED = 300.0
 
 var target = null
+var health: int = 100
 
 func _physics_process(delta: float) -> void:
 	if target != null:
@@ -13,10 +14,20 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity = Vector2.ZERO
 
+func take_damage(damage: int) -> void:
+	health -= damage
+	print(health)
+	if health <= 0:
+		die()
+
+func die() -> void:
+	queue_free()
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("bullets"):
 		print("Damage enemy")
+		take_damage(body.damage)
+		body.add_collision_exception_with(self)
 
 
 func _on_sight_body_entered(body: Node2D) -> void:
